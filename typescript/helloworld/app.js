d053c898289044e5af0f9aa718c262dfc5072611
AD <silentflute.ad@gmail.com>
2024-09-04 13:21:38 +0100

    adding some more examples

--- typescript/helloworld/app.js
+++ typescript/helloworld/app.js
@@ -4,4 +4,30 @@ var heading = document.createElement('h1');
 heading.textContent = message;
 // add to document
 document.body.appendChild(heading);
-
+// counter
+var counter = 0;
+function increment(counter) {
+    return counter++;
+}
+var countup = increment(counter);
+console.log(countup);
+// string interpolations
+var firstName = "John";
+var title = "Web Developer";
+var profile = "I'm ".concat(firstName, ". \nI'm a ").concat(title);
+console.log(profile);
+// object
+var employee;
+employee = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+console.log(employee);
+// arrays
+var skills = [];
+skills[0] = "Problem Solving";
+skills[1] = "Programming";
+skills.push('Software Design');
+console.log(skills);
--- typescript/helloworld/app.ts
+++ typescript/helloworld/app.ts
@@ -7,3 +7,41 @@ heading.textContent = message;
 // add to document
 document.body.appendChild(heading);
 
+// counter
+let counter: number = 0;
+
+function increment(counter: number): number {
+    return counter++;
+}
+
+let countup = increment(counter);
+
+console.log(countup);
+
+// string interpolations
+let firstName: string = `John`;
+let title: string = `Web Developer`;
+let profile: string = `I'm ${firstName}. 
+I'm a ${title}`;
+
+console.log(profile);
+
+// object
+let employee: object;
+
+employee = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+
+console.log(employee);
+
+// arrays
+let skills: string[] = [];
+skills[0] = "Problem Solving";
+skills[1] = "Programming";
+skills.push('Software Design');
+
+console.log(skills);
